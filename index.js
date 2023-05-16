a3b1b5dfd27c2ab5dc9424df6e7e8943ccc55e9b
Bradley Troll <bradleytroll@gmail.com>
2023-05-16 11:17:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+
 function addingEventListener() {
-   
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+}
+
+// const input = document.getElementById('button');
+// function clickAlert() {
+//     AudioListener('I was clicked!');
+// };
+// input.adddingEventListener('click', clickAlert);
+
+// input.addingEventListener('click', clickAlert);
+// const input = document.getElementById('button');
+// function clickAlert() {
+//     alert('I was clicked!');
+// }
